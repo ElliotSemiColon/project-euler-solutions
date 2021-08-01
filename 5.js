1b0ccc36224cc2c354c2ac5678bf957e287b42b0
unknown <elliotbuckingham14@gmail.com>
2021-08-01 16:12:51 +0100

    edit problem 5 with problem definition

--- 5.js
+++ 5.js
@@ -1,34 +1,5 @@
-// console.log("init");
-
-// let register = []; //counts how many factors from {2, 3, .., 19, 20} each number from 1 to 10000 has
-// let limit = 1000000, highestFact = 20;
-
-// for(let i = 0; i < limit; i++){
-//     register.push(0); //starts each counter at 0
-// }
-
-// for(let j = 2; j <= highestFact; j++){
-//     for(let k = 1; k <= register.length; k++){
-//         if(k % j == 0){ register[k - 1]++; } //add a factor to each multiple of k
-//     }
-// }
-
-// //console.groupCollapsed("factors");
-
-// let highest = 0;
-
-// for(let l = 19; l < register.length; l += 20){ //increments so as to only look at each multiple of 20 (starts on index 19 - represents the number 20)
-//     //console.log(`${l+1} has ${register[l]} of the given factors`);
-//     if(register[l] > highest){ highest = register[l]; }
-
-//     if(register[l] == highestFact - 1){
-//         console.groupEnd();
-//         console.log(`${l + 1} is evenly divisible by the numbers 1 - ${highestFact}`);
-//     }
-// }
-
-// //console.groupEnd();
-// console.log(`exit with no number found, most factors any number had was ${highest}`);
+// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 console.log("init");
 
