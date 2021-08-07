7341c71a93e5756c36c2dedfde35df0776ddc2f6
unknown <elliotbuckingham14@gmail.com>
2021-08-08 00:41:56 +0100

    problem 13

---
+++ 10-19/p13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace p13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sequence = File.ReadAllLines(@"B:\Elliot Buckingham\Documents\Visual Studio 2019\repos\project-euler\project-euler\13.txt");
+
+            //Console.WriteLine($"file data:\n{string.Join("\n", sequence)}");
+
+            ulong sum = 0, n = 0;
+            int precision = 15; //how many digits to add per number
+
+            foreach (string line in sequence)
+            {
+                n = ulong.Parse(line.Substring(0, precision)); 
+                sum += n;
+            }
+
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
